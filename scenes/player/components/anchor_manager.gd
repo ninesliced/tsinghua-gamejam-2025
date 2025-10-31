98bd68913061a35531b261c16830fb17ba64e728
Guillame Tran <guillaume1.tran@epitech.eu>
2025-10-31 22:36:42 +0900

    add anchor and anchor manager wip

--- scenes/player/components/anchor_manager.gd
+++ scenes/player/components/anchor_manager.gd
@@ -2,7 +2,26 @@ extends Node
 class_name AnchorManager
 
 @export var anchor_max = 3
-@export var list_anchors: = []
+@export var list_anchors: Array[Anchor] = []
+@onready var anchor_scene: PackedScene = preload("res://scenes/anchors/anchor.tscn")
 
 func _ready():
-    pass
+	for i in range(anchor_max):
+		var anchor = anchor_scene.instantiate() as Anchor
+		list_anchors.append(anchor)
+	pass
+
+func use_anchor():
+	if list_anchors.size() < 1:
+		return
+	var anchor = list_anchors.pop_back()
+	get_tree().get_root().add_child(anchor)
+	anchor.set_process(true)
+
+
+func add_anchor(anchor: Anchor):
+	if list_anchors.size() >= anchor_max:
+		return
+	list_anchors.append(anchor)
+	self.add_child(anchor)
+	anchor.set_process(false)
---
+++ scenes/player/states/normal.gd
@@ -0,0 +1,18 @@
+extends State
+
+@export var anchor_manager : AnchorManager
+
+func on_entity_set():
+	pass
+
+func enter():
+	pass
+
+func process(delta):
+	pass
+
+func physics_process(delta):
+	pass
+
+func exit():
+	pass
