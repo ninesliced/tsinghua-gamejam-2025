2afb54dd34de5db833686040cfd54775029bfc37
TheodoreBillotte <theodore.billotte@epitech.eu>
2025-11-01 19:15:08 +0800

    add: spawner

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -2,7 +2,6 @@ extends CharacterBody2D
 class_name Player
 
 @onready var anchor_manager: AnchorManager = %AnchorManager
-@onready var energy_component: EnergyComponent = %EnergyComponent
 
 func _physics_process(delta):
 	move_and_slide()
