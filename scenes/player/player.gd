bcfb40a948681a87845793bbf25214c3bb588d06
TheodoreBillotte <theodore.billotte@epitech.eu>
2025-11-01 17:41:35 +0800

    add: energy component to the player

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -1,6 +1,8 @@
 extends CharacterBody2D
 class_name Player
+
 @onready var anchor_manager: AnchorManager = %AnchorManager
+@onready var energy_component: EnergyComponent = %EnergyComponent
 
 func _physics_process(delta):
 	move_and_slide()
