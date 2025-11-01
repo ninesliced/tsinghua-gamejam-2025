5e4d44eda4fd44e70be07711e5f1005a4e2ea4e8
Louis Rollet <louis.rollet@epitech.eu>
2025-11-01 22:44:37 +0800

    [FIX] anchor on generators and chest error

--- scenes/collectibles/chest.gd
+++ scenes/collectibles/chest.gd
@@ -10,10 +10,11 @@ func _on_interaction_area_body_entered(body: Node) -> void:
 	if body is Player and !is_open:
 		is_open = true
 		animated_sprite_2d.play("open")
-		item_data.shuffle()
-		var selected_item = item_data[0]
-		print("Player collected item from chest:", selected_item)
-		#body.inventory.add_item(selected_item)
+		if (len(item_data) > 0):
+			item_data.shuffle()
+			var selected_item = item_data[0]
+			print("Player collected item from chest:", selected_item)
+			#body.inventory.add_item(selected_item)
 
 func _ready() -> void:
 	interaction_area.body_entered.connect(_on_interaction_area_body_entered)
