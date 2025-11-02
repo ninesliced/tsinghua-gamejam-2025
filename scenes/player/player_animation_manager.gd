00e3e8e789709b1c1a33a878b70a9a9c8b94dd1a
Guillame Tran <guillaume1.tran@epitech.eu>
2025-11-02 16:47:13 +0900

    add other particles to player

--- scenes/player/player_animation_manager.gd
+++ scenes/player/player_animation_manager.gd
@@ -4,6 +4,7 @@ class_name PlayerAnimationManager
 
 @export var display_node : Node2D
 @export var particules_node : CPUParticles2D
+@export var dirt_particules_node : CPUParticles2D
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -19,12 +20,14 @@ func _process(delta):
 func _on_movement_component_on_stop():
 	play("idle", 1)
 	particules_node.emitting = false
+	dirt_particules_node.emitting = false
 	#await 3 seconds
 	await get_tree().create_timer(0.2).timeout
 	pass # Replace with function body.
 
 func _on_movement_component_on_move(direction: Vector2):
 	particules_node.emitting = true
+	dirt_particules_node.emitting = true
 	if (direction.y != 0):
 		if direction.y < 0:
 			play("up")
